79fe352e8630d42399ef745406ac4f2da7a2e172
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-27 19:36:19 +0700

    feat: enhance calculator function to support parentheses and update tests

--- src/utils/calculator.rs
+++ src/utils/calculator.rs
@@ -1,7 +1,10 @@
+use std::collections::VecDeque;
+
 pub(crate) fn calculate(expression: &str) -> anyhow::Result<i32> {
-    let mut result: i32 = 0;
+    let mut stack: VecDeque<(i32, i32)> = VecDeque::new();
     let n = expression.len();
     let expr_bytes = expression.as_bytes();
+    let mut result: i32 = 0;
     let mut curr_num: i32 = 0;
     let mut curr_sign: i32 = 1;
     let mut i = 0;
@@ -12,6 +15,17 @@ pub(crate) fn calculate(expression: &str) -> anyhow::Result<i32> {
             i += 1;
         } else if expr_bytes[i] == b'+' {
             curr_sign = 1;
+            i += 1;
+        } else if expr_bytes[i] == b'(' {
+            result += curr_num;
+            stack.push_back((result, curr_sign));
+            result = 0;
+            curr_num = 0;
+            curr_sign = 1;
+            i += 1;
+        } else if expr_bytes[i] == b')' {
+            let (last_result, last_sign) = stack.pop_back().unwrap();
+            result = last_result + last_sign * result;
             i += 1;
         } else if expr_bytes[i].is_ascii_digit() {
             while i < n
@@ -47,6 +61,10 @@ mod tests {
     #[case("1-1+2", anyhow::Ok(2))]
     #[case("+4-2+10", anyhow::Ok(12))]
     #[case("-15+10", anyhow::Ok(-5))]
+    #[case("12+8-(10+2)", anyhow::Ok(8))]
+    #[case("2-(5-6)", anyhow::Ok(3))]
+    #[case("2-(5-6)+3", anyhow::Ok(6))]
+    #[case("2-(5-6)+3-(4+2)", anyhow::Ok(0))]
     fn test_calculate(#[case] expression: &str, #[case] expected: anyhow::Result<i32>) {
         let actual = calculate(expression);
         assert_eq!(actual.unwrap(), expected.unwrap());
